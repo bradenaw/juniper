aea2ad3c20f23d95d7d13db4af126fa84eb20948
Braden Walker <bradenawalker@gmail.com>
2021-12-09 12:39:00 -0800

    parent/children test for heap

---
+++ internal/heap/heap_test.go
@@ -0,0 +1,27 @@
+package heap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParentChildren(t *testing.T) {
+	require.Equal(t, 0, parent(1))
+	require.Equal(t, 0, parent(2))
+	left, right := children(0)
+	require.Equal(t, 1, left)
+	require.Equal(t, 2, right)
+
+	require.Equal(t, 1, parent(3))
+	require.Equal(t, 1, parent(4))
+	left, right = children(1)
+	require.Equal(t, 3, left)
+	require.Equal(t, 4, right)
+
+	require.Equal(t, 2, parent(5))
+	require.Equal(t, 2, parent(6))
+	left, right = children(2)
+	require.Equal(t, 5, left)
+	require.Equal(t, 6, right)
+}
