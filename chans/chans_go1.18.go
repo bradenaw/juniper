d9c17006b1b5ce8e78ebee9ba5dbf0e5feaa8e3b
Braden Walker <bradenawalker@gmail.com>
2021-12-16 22:55:46 -0800

    SendOrExpire,RecvOrExpire,SleepOrExpire

---
+++ chans/chans_go1.18.go
@@ -0,0 +1,27 @@
+//go:build go1.18
+package chans
+
+import "context"
+
+// SendOrExpire sends item on channel c and returns nil, unless ctx expires in which case it returns
+// ctx.Err().
+func SendOrExpire[T any](ctx context.Context, c chan<- T, item T) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case c<-item:
+		return nil
+	}
+}
+
+// RecvOrExpire attempts to receive from channel c. If c is closed before or during, returns (_,
+// false, nil). If ctx expires before or during, returns (_, _, ctx.Err()).
+func RecvOrExpire[T any](ctx context.Context, c <-chan T) (T, bool, error) {
+	select {
+	case <-ctx.Done():
+		var zero T
+		return zero, false, ctx.Err()
+	case item, ok := <-c:
+		return item, ok, nil
+	}
+}
---
+++ xtime/xtime.go
@@ -0,0 +1,24 @@
+package xtime
+
+import (
+	"context"
+	"time"
+)
+
+// SleepOrExpire pauses the current goroutine for at least the duration d and returns nil, unless
+// ctx expires in the mean time in which case it returns ctx.Err().
+//
+// A negative or zero duration causes SleepOrExpire to return immediately.
+func SleepOrExpire(ctx context.Context, d time.Duration) error {
+	if d < 0 {
+		return nil
+	}
+	t := time.NewTimer(d)
+	select {
+	case <-ctx.Done():
+		t.Stop()
+		return ctx.Err()
+	case <-t.C:
+		return nil
+	}
+}
