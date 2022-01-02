642c66bb0f923b4c39908e4c590f997f76ca1ea4
Braden Walker <bradenawalker@gmail.com>
2022-01-01 23:07:25 -0800

    add xsync.{Map,Pool}

--- xsync/xsync_go1.18.go
+++ xsync/xsync_go1.18.go
@@ -16,3 +16,46 @@ func Lazy[T any](f func() T) func() T {
 		return val
 	}
 }
+
+// Map is a typesafe wrapper over sync.Map.
+type Map[K comparable, V any] struct {
+	m sync.Map
+}
+
+func (m *Map[K, V]) Delete(key K) {
+	m.m.Delete(key)
+}
+func (m *Map[K, V]) Load(key K) (value V, ok bool) {
+	value_, ok := m.m.Load(key)
+	return value_.(V), ok
+}
+func (m *Map[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+	value_, loaded := m.m.LoadAndDelete(key)
+	return value_.(V), loaded
+}
+func (m *Map[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
+	actual_, loaded := m.m.LoadOrStore(key, value)
+	return actual_.(V), loaded
+}
+func (m *Map[K, V]) Range(f func(key K, value V) bool) {
+	m.m.Range(func(key, value interface{}) bool {
+		return f(key.(K), value.(V))
+	})
+}
+func (m *Map[K, V]) Store(key K, value V) {
+	m.m.Store(key, value)
+}
+
+// Pool is a typesafe wrapper over sync.Pool.
+type Pool[T any] struct {
+	New func() T
+	p sync.Pool
+}
+
+func (p *Pool[T]) Get() T {
+	return p.p.Get().(T)
+}
+
+func (p *Pool[T]) Put(x T) {
+	p.p.Put(x)
+}
