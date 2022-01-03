cffa8fdec68c0b21d0a5b63efc7d89bd3a4492c5
Braden Walker <bradenawalker@gmail.com>
2022-01-02 23:49:46 -0800

    xtime.SleepContext checks deadline and early returns

--- xtime/xtime.go
+++ xtime/xtime.go
@@ -5,13 +5,35 @@ import (
 	"time"
 )
 
+type DeadlineTooSoonError struct {
+	remaining time.Duration
+	d         time.Duration
+}
+
+func (err DeadlineTooSoonError) Error() string {
+	return fmt.Sprintf(
+		"not enough time remaining in context: %s remaining for %s sleep",
+		err.remaining,
+		err.d,
+	)
+}
+
 // SleepContext pauses the current goroutine for at least the duration d and returns nil, unless ctx
 // expires in the mean time in which case it returns ctx.Err().
 //
 // A negative or zero duration causes SleepContext to return immediately.
+//
+// If there is less than d left until ctx's deadline, returns DeadlineTooSoonError immediately.
 func SleepContext(ctx context.Context, d time.Duration) error {
-	if d < 0 {
+	if d <= 0 {
 		return nil
+	}
+	dealine, ok := ctx.Deadline()
+	if ok {
+		remaining := time.Until(deadline)
+		if remaining > d {
+			return DeadlineTooSoonError{remaining: remaining, d: d}
+		}
 	}
 	t := time.NewTimer(d)
 	select {
