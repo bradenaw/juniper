6c075528c5044e36defacecc0646e84c5cf8a5df
Braden Walker <bradenawalker@gmail.com>
2022-01-11 13:51:12 -0800

    sort slices.go and friends alphabetically, separate tests from examples

---
+++ slices/slices_example_go1.18_test.go
@@ -0,0 +1,277 @@
+//go:build go1.18
+
+package slices_test
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/bradenaw/juniper/slices"
+	"github.com/bradenaw/juniper/xmath"
+)
+
+func ExampleAll() {
+	isOdd := func(x int) bool {
+		return x%2 != 0
+	}
+
+	allOdd := slices.All([]int{1, 3, 5}, isOdd)
+	fmt.Println(allOdd)
+
+	allOdd = slices.All([]int{1, 3, 6}, isOdd)
+	fmt.Println(allOdd)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleAny() {
+	isOdd := func(x int) bool {
+		return x%2 != 0
+	}
+
+	anyOdd := slices.Any([]int{2, 3, 4}, isOdd)
+	fmt.Println(anyOdd)
+
+	anyOdd = slices.Any([]int{2, 4, 6}, isOdd)
+	fmt.Println(anyOdd)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleClear() {
+	x := []int{1, 2, 3}
+	slices.Clear(x)
+	fmt.Println(x)
+
+	// Output:
+	// [0 0 0]
+}
+
+func ExampleChunk() {
+	a := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	chunks := slices.Chunk(a, 3)
+	fmt.Println(chunks)
+
+	// Output:
+	// [[a b c] [d e f] [g h]]
+}
+
+func ExampleClone() {
+	x := []int{1, 2, 3}
+	cloned := slices.Clone(x)
+	fmt.Println(cloned)
+
+	// Output:
+	// [1 2 3]
+}
+
+func ExampleCompact() {
+	x := []string{"a", "a", "b", "c", "c", "c", "a"}
+	compacted := slices.Compact(x)
+	fmt.Println(compacted)
+
+	// Output:
+	// [a b c a]
+}
+
+func ExampleCompactFunc() {
+	x := []string{
+		"bank",
+		"beach",
+		"ghost",
+		"goat",
+		"group",
+		"yaw",
+		"yew",
+	}
+	compacted := slices.CompactFunc(x, func(a, b string) bool {
+		return a[0] == b[0]
+	})
+	fmt.Println(compacted)
+
+	// Output:
+	// [bank ghost yaw]
+}
+
+func ExampleCount() {
+	x := []string{"a", "b", "a", "a", "b"}
+
+	fmt.Println(slices.Count(x, "a"))
+
+	// Output:
+	// 3
+}
+
+func ExampleEqual() {
+	x := []string{"a", "b", "c"}
+	y := []string{"a", "b", "c"}
+	z := []string{"a", "b", "d"}
+
+	fmt.Println(slices.Equal(x, y))
+	fmt.Println(slices.Equal(x[:2], y))
+	fmt.Println(slices.Equal(z, y))
+
+	// Output:
+	// true
+	// false
+	// false
+}
+
+func ExampleFilter() {
+	x := []int{5, -9, -2, 1, -4, 8, 3}
+	x = slices.Filter(x, func(value int) bool {
+		return value > 0
+	})
+	fmt.Println(x)
+
+	// Output:
+	// [5 1 8 3]
+}
+
+func ExampleGrow() {
+	x := make([]int, 0, 1)
+	x = slices.Grow(x, 4)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	x = append(x, 1)
+	addr := &x[0]
+	x = append(x, 2)
+	fmt.Println(addr == &x[0])
+	x = append(x, 3)
+	fmt.Println(addr == &x[0])
+	x = append(x, 4)
+	fmt.Println(addr == &x[0])
+
+	// Output:
+	// 0
+	// 4
+	// true
+	// true
+	// true
+}
+
+func ExampleIndex() {
+	x := []string{"a", "b", "a", "a", "b"}
+
+	fmt.Println(slices.Index(x, "b"))
+	fmt.Println(slices.Index(x, "c"))
+
+	// Output:
+	// 1
+	// -1
+}
+
+func ExampleInsert() {
+	x := []string{"a", "b", "c", "d", "e"}
+	x = slices.Insert(x, 3, "f", "g")
+	fmt.Println(x)
+
+	// Output:
+	// [a b c f g d e]
+}
+
+func ExampleJoin() {
+	joined := slices.Join(
+		[]string{"a", "b", "c"},
+		[]string{"x", "y"},
+		[]string{"l", "m", "n", "o"},
+	)
+
+	fmt.Println(joined)
+
+	// Output:
+	// [a b c x y l m n o]
+}
+
+func ExampleLastIndex() {
+	x := []string{"a", "b", "a", "a", "b"}
+
+	fmt.Println(slices.LastIndex(x, "a"))
+	fmt.Println(slices.LastIndex(x, "c"))
+
+	// Output:
+	// 3
+	// -1
+}
+
+func ExampleMap() {
+	toHalfFloat := func(x int) float32 {
+		return float32(x) / 2
+	}
+
+	a := []int{1, 2, 3}
+	floats := slices.Map(a, toHalfFloat)
+	fmt.Println(floats)
+
+	// Output:
+	// [0.5 1 1.5]
+}
+
+func ExamplePartition() {
+	a := []int{11, 3, 4, 2, 7, 8, 0, 1, 14}
+
+	slices.Partition(a, func(x int) bool { return x%2 == 0 })
+
+	fmt.Println(a)
+
+	// Output:
+	// [11 3 1 7 2 8 0 4 14]
+}
+
+func ExampleRemove() {
+	x := []int{1, 2, 3, 4, 5}
+	x = slices.Remove(x, 1, 2)
+	fmt.Println(x)
+
+	// Output:
+	// [1 4 5]
+}
+
+func ExampleReverse() {
+	x := []string{"a", "b", "c", "d", "e"}
+	slices.Reverse(x)
+	fmt.Println(x)
+
+	// Output:
+	// [e d c b a]
+}
+
+func ExampleReduce() {
+	x := []int{3, 1, 2}
+
+	sum := slices.Reduce(x, 0, func(x, y int) int { return x + y })
+	fmt.Println(sum)
+
+	min := slices.Reduce(x, math.MaxInt, xmath.Min[int])
+	fmt.Println(min)
+
+	// Output:
+	// 6
+	// 1
+}
+
+func ExampleRuns() {
+	x := []int{2, 4, 0, 7, 1, 3, 9, 2, 8}
+
+	parityRuns := slices.Runs(x, func(a, b int) bool {
+		return a%2 == b%2
+	})
+
+	fmt.Println(parityRuns)
+
+	// Output:
+	// [[2 4 0] [7 1 3 9] [2 8]]
+}
+
+func ExampleUnique() {
+	a := []string{"a", "b", "b", "c", "a", "b", "b", "c"}
+	unique := slices.Unique(a)
+	fmt.Println(unique)
+
+	// Output:
+	// [a b c]
+}
--- slices/slices_go1.18.go
+++ slices/slices_go1.18.go
@@ -2,71 +2,42 @@
 
 package slices
 
-// Grow grows x's capacity by reallocating, if necessary, to fit n more elements and returns the
-// modified slice. This does not change the length of x. After Grow(x, n), the following n
-// append()s to x will not need to reallocate.
-func Grow[T any](x []T, n int) []T {
-	if cap(x)-len(x) < n {
-		x2 := make([]T, len(x)+n)
-		copy(x2, x)
-		return x2[:len(x)]
-	}
-	return x
-}
 
-// Shrink shrinks x's capacity by reallocating, if necessary, so that cap(x) <= len(x) + n.
-func Shrink[T any](x []T, n int) []T {
-	if cap(x) > len(x)+n {
-		x2 := make([]T, len(x)+n)
-		copy(x2, x)
-		return x2[:len(x)]
-	}
-	return x
-}
-
-// Filter filters the contents of x to only those for which keep() returns true. This is done
-// in-place and so modifies the contents of x. The modified slice is returned.
-func Filter[T any](x []T, keep func(t T) bool) []T {
-	filtered := x[:0]
+// All returns true if f(x[i]) returns true for all i. Trivially, returns true if x is empty.
+func All[T any](x []T, f func(T) bool) bool {
 	for i := range x {
-		if keep(x[i]) {
-			filtered = append(filtered, x[i])
+		if !f(x[i]) {
+			return false
 		}
 	}
-	// Zero out the rest in case they contain pointers, so that filtered doesn't retain references.
-	Clear(x[len(filtered):])
-	return filtered
+	return true
 }
 
-// Reverse reverses the elements of x in place.
-func Reverse[T any](x []T) {
-	for i := 0; i < len(x)/2; i++ {
-		x[i], x[len(x)-i-1] = x[len(x)-i-1], x[i]
+// Any returns true if f(x[i]) returns true for any i. Trivially, returns false if x is empty.
+func Any[T any](x []T, f func(T) bool) bool {
+	for i := range x {
+		if f(x[i]) {
+			return true
+		}
 	}
+	return false
 }
 
-// Insert inserts the given values starting at index idx, shifting elements after idx to the right
-// and growing the slice to make room. Insert will expand the length of the slice up to its capacity
-// if it can, if this isn't desired then x should be resliced to have capacity equal to its length:
+// Chunk returns non-overlapping chunks of x. The last chunk will be smaller than chunkSize if
+// len(x) is not a multiple of chunkSize.
 //
-//   x[:len(x):len(x)]
-//
-// The time cost is O(n+m) where n is len(values) and m is len(x[idx:]).
-func Insert[T any](x []T, idx int, values ...T) []T {
-	x = Grow(x, len(values))
-	x = x[: len(x)+len(values) : len(x)+len(values)]
-	copy(x[idx+len(values):], x[idx:])
-	copy(x[idx:], values)
-	return x
-}
-
-// Remove removes n elements from x starting at index idx and returns the modified slice. This
-// requires shifting the elements after the removed elements over, and so its cost is linear in the
-// number of elements shifted.
-func Remove[T any](x []T, idx int, n int) []T {
-	copy(x[idx:], x[idx+n:])
-	Clear(x[len(x)-n:])
-	return x[:len(x)-n]
+// Returns an empty slice if len(x)==0. Panics if chunkSize <= 0.
+func Chunk[T any](x []T, chunkSize int) [][]T {
+	out := make([][]T, (len(x)+chunkSize-1)/chunkSize)
+	for i := range out {
+		start := i * chunkSize
+		end := (i + 1) * chunkSize
+		if end > len(x) {
+			end = len(x)
+		}
+		out[i] = x[start:end]
+	}
+	return out
 }
 
 // Clear fills x with the zero value of T.
@@ -107,19 +78,6 @@ func CompactFunc[T any](x []T, eq func(T, T) bool) []T {
 	return compacted
 }
 
-// Equal returns true if a and b contain the same items in the same order.
-func Equal[T comparable](a, b []T) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
 // Count returns the number of times item appears in a.
 func Count[T comparable](a []T, item T) int {
 	return CountFunc(a, func(x T) bool { return item == x })
@@ -134,6 +92,45 @@ func CountFunc[T any](a []T, f func(T) bool) int {
 		}
 	}
 	return n
+}
+
+// Equal returns true if a and b contain the same items in the same order.
+func Equal[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// Filter filters the contents of x to only those for which keep() returns true. This is done
+// in-place and so modifies the contents of x. The modified slice is returned.
+func Filter[T any](x []T, keep func(t T) bool) []T {
+	filtered := x[:0]
+	for i := range x {
+		if keep(x[i]) {
+			filtered = append(filtered, x[i])
+		}
+	}
+	// Zero out the rest in case they contain pointers, so that filtered doesn't retain references.
+	Clear(x[len(filtered):])
+	return filtered
+}
+
+// Grow grows x's capacity by reallocating, if necessary, to fit n more elements and returns the
+// modified slice. This does not change the length of x. After Grow(x, n), the following n
+// append()s to x will not need to reallocate.
+func Grow[T any](x []T, n int) []T {
+	if cap(x)-len(x) < n {
+		x2 := make([]T, len(x)+n)
+		copy(x2, x)
+		return x2[:len(x)]
+	}
+	return x
 }
 
 // Index returns the first index of item in a, or -1 if item is not in a.
@@ -157,6 +154,34 @@ func IndexFunc[T any](a []T, f func(T) bool) int {
 	return -1
 }
 
+// Insert inserts the given values starting at index idx, shifting elements after idx to the right
+// and growing the slice to make room. Insert will expand the length of the slice up to its capacity
+// if it can, if this isn't desired then x should be resliced to have capacity equal to its length:
+//
+//   x[:len(x):len(x)]
+//
+// The time cost is O(n+m) where n is len(values) and m is len(x[idx:]).
+func Insert[T any](x []T, idx int, values ...T) []T {
+	x = Grow(x, len(values))
+	x = x[: len(x)+len(values) : len(x)+len(values)]
+	copy(x[idx+len(values):], x[idx:])
+	copy(x[idx:], values)
+	return x
+}
+
+// Join joins together the contents of each in.
+func Join[T any](in ...[]T) []T {
+	n := 0
+	for i := range in {
+		n += len(in[i])
+	}
+	out := make([]T, 0, n)
+	for i := range in {
+		out = append(out, in[i]...)
+	}
+	return out
+}
+
 // LastIndex returns the last index of item in a, or -1 if item is not in a.
 func LastIndex[T comparable](a []T, item T) int {
 	for i := len(a) - 1; i >= 0; i-- {
@@ -178,15 +203,11 @@ func LastIndexFunc[T any](a []T, f func(T) bool) int {
 	return -1
 }
 
-// Join joins together the contents of each in.
-func Join[T any](in ...[]T) []T {
-	n := 0
-	for i := range in {
-		n += len(in[i])
-	}
-	out := make([]T, 0, n)
-	for i := range in {
-		out = append(out, in[i]...)
+// Map creates a new slice by applying f to each element of x.
+func Map[T any, U any](x []T, f func(T) U) []U {
+	out := make([]U, len(x))
+	for i := range x {
+		out[i] = f(x[i])
 	}
 	return out
 }
@@ -221,70 +242,6 @@ func Partition[T any](x []T, f func(t T) bool) {
 	}
 }
 
-// Unique removes duplicates from x in-place, preserving order, and returns the modified slice.
-//
-// Compact is more efficient if duplicates are already adjacent in x, for example if x is in sorted
-// order.
-func Unique[T comparable](x []T) []T {
-	filtered := x[:0]
-	m := make(map[T]struct{}, len(x))
-	for i := range x {
-		_, ok := m[x[i]]
-		if !ok {
-			filtered = append(filtered, x[i])
-			m[x[i]] = struct{}{}
-		}
-	}
-	Clear(x[len(filtered):])
-	return filtered
-}
-
-// Chunk returns non-overlapping chunks of x. The last chunk will be smaller than chunkSize if
-// len(x) is not a multiple of chunkSize.
-//
-// Returns an empty slice if len(x)==0. Panics if chunkSize <= 0.
-func Chunk[T any](x []T, chunkSize int) [][]T {
-	out := make([][]T, (len(x)+chunkSize-1)/chunkSize)
-	for i := range out {
-		start := i * chunkSize
-		end := (i + 1) * chunkSize
-		if end > len(x) {
-			end = len(x)
-		}
-		out[i] = x[start:end]
-	}
-	return out
-}
-
-// Any returns true if f(x[i]) returns true for any i. Trivially, returns false if x is empty.
-func Any[T any](x []T, f func(T) bool) bool {
-	for i := range x {
-		if f(x[i]) {
-			return true
-		}
-	}
-	return false
-}
-
-// All returns true if f(x[i]) returns true for all i. Trivially, returns true if x is empty.
-func All[T any](x []T, f func(T) bool) bool {
-	for i := range x {
-		if !f(x[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-// Map creates a new slice by applying f to each element of x.
-func Map[T any, U any](x []T, f func(T) U) []U {
-	out := make([]U, len(x))
-	for i := range x {
-		out[i] = f(x[i])
-	}
-	return out
-}
-
 // Reduce reduces x to a single value using the reduction function f.
 func Reduce[T any, U any](x []T, initial U, f func(U, T) U) U {
 	out := initial
@@ -292,6 +249,22 @@ func Reduce[T any, U any](x []T, initial U, f func(U, T) U) U {
 		out = f(out, x[i])
 	}
 	return out
+}
+
+// Remove removes n elements from x starting at index idx and returns the modified slice. This
+// requires shifting the elements after the removed elements over, and so its cost is linear in the
+// number of elements shifted.
+func Remove[T any](x []T, idx int, n int) []T {
+	copy(x[idx:], x[idx+n:])
+	Clear(x[len(x)-n:])
+	return x[:len(x)-n]
+}
+
+// Reverse reverses the elements of x in place.
+func Reverse[T any](x []T) {
+	for i := 0; i < len(x)/2; i++ {
+		x[i], x[len(x)-i-1] = x[len(x)-i-1], x[i]
+	}
 }
 
 // Runs returns a slice of slices. The inner slices are contiguous runs of elements from x such
@@ -318,4 +291,32 @@ func Runs[T any](x []T, same func(a, b T) bool) [][]T {
 		runs = append(runs, x[start:])
 	}
 	return runs
+}
+
+// Shrink shrinks x's capacity by reallocating, if necessary, so that cap(x) <= len(x) + n.
+func Shrink[T any](x []T, n int) []T {
+	if cap(x) > len(x)+n {
+		x2 := make([]T, len(x)+n)
+		copy(x2, x)
+		return x2[:len(x)]
+	}
+	return x
+}
+
+// Unique removes duplicates from x in-place, preserving order, and returns the modified slice.
+//
+// Compact is more efficient if duplicates are already adjacent in x, for example if x is in sorted
+// order.
+func Unique[T comparable](x []T) []T {
+	filtered := x[:0]
+	m := make(map[T]struct{}, len(x))
+	for i := range x {
+		_, ok := m[x[i]]
+		if !ok {
+			filtered = append(filtered, x[i])
+			m[x[i]] = struct{}{}
+		}
+	}
+	Clear(x[len(filtered):])
+	return filtered
 }
