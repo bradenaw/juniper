0d7166c5e967835aa7c72022f9ef31bb3e057e54
Braden Walker <bradenawalker@gmail.com>
2022-01-11 17:50:16 -0800

    parallel.Map{Iterator,Stream}

--- parallel/parallel_go1.18.go
+++ parallel/parallel_go1.18.go
@@ -5,6 +5,14 @@ package parallel
 
 import (
 	"context"
+	"runtime"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/bradenaw/juniper/iterator"
+	"github.com/bradenaw/juniper/slices"
+	"github.com/bradenaw/juniper/stream"
+	"github.com/bradenaw/juniper/xsort"
 )
 
 // Map uses parallelism goroutines to call f once for each element of in. out[i] is the
@@ -46,4 +54,178 @@ func MapContext[T any, U any](
 		return nil, err
 	}
 	return out, nil
+}
+
+// MapIterator uses parallelism goroutines to call f once for each element yielded by iter. The
+// returned iterator returns these results in the same order that iter produced them in.
+//
+// If parallelism <= 0, uses GOMAXPROCS instead.
+//
+// bufferSize is the size of the work buffer for each goroutine. A larger buffer uses more memory
+// but gives better throughput in the face of larger variance in the processing time for f.
+func MapIterator[T any, U any](
+	iter iterator.Iterator[T],
+	parallelism int,
+	bufferSize int,
+	f func(T) U,
+) iterator.Iterator[U] {
+	if parallelism <= 0 {
+		parallelism = runtime.GOMAXPROCS(-1)
+	}
+
+	in := make(chan valueAndIndex[T])
+
+	go func() {
+		i := 0
+		for {
+			item, ok := iter.Next()
+			if !ok {
+				break
+			}
+
+			in <- valueAndIndex[T]{
+				value: item,
+				idx:   i,
+			}
+			i++
+		}
+		close(in)
+	}()
+
+	outs := make([]chan valueAndIndex[U], parallelism)
+	for i := 0; i < parallelism; i++ {
+		i := i
+		outs[i] = make(chan valueAndIndex[U], bufferSize)
+		go func() {
+			for item := range in {
+				u := f(item.value)
+				outs[i] <- valueAndIndex[U]{value: u, idx: item.idx}
+			}
+			close(outs[i])
+		}()
+	}
+
+	return iterator.Map(
+		xsort.Merge(
+			func(a, b valueAndIndex[U]) bool {
+				return a.idx < b.idx
+			},
+			slices.Map(outs, func(c chan valueAndIndex[U]) iterator.Iterator[valueAndIndex[U]] {
+				return iterator.Chan(c)
+			})...,
+		),
+		func(x valueAndIndex[U]) U {
+			return x.value
+		},
+	)
+}
+
+type valueAndIndex[T any] struct {
+	value T
+	idx   int
+}
+
+// MapStream uses parallelism goroutines to call f once for each element yielded by s. The returned
+// iterator returns these results in the same order that iter produced them in.
+//
+// If any call to f returns an error the context passed to invocations of f is cancelled, no further
+// calls to f are made, and the returned stream's Next returns the first error encountered.
+//
+// If parallelism <= 0, uses GOMAXPROCS instead.
+//
+// bufferSize is the size of the work buffer for each goroutine. A larger buffer uses more memory
+// but gives better throughput in the face of larger variance in the processing time for f.
+func MapStream[T any, U any](
+	s stream.Stream[T],
+	parallelism int,
+	bufferSize int,
+	f func(context.Context, T) (U, error),
+) stream.Stream[U] {
+	if parallelism <= 0 {
+		parallelism = runtime.GOMAXPROCS(-1)
+	}
+
+	in := make(chan valueAndIndex[T])
+
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		defer s.Close()
+		defer close(in)
+		i := 0
+		for {
+			item, err := s.Next(ctx)
+			if err == stream.End {
+				break
+			} else if err != nil {
+				return err
+			}
+
+			select {
+			case in <- valueAndIndex[T]{
+				value: item,
+				idx:   i,
+			}:
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+			i++
+		}
+		return nil
+	})
+
+	outs := make([]chan valueAndIndex[U], parallelism)
+	for i := 0; i < parallelism; i++ {
+		i := i
+		outs[i] = make(chan valueAndIndex[U], bufferSize)
+		eg.Go(func() error {
+			defer close(outs[i])
+			for item := range in {
+				u, err := f(ctx, item.value)
+				if err != nil {
+					return err
+				}
+				select {
+				case outs[i] <- valueAndIndex[U]{value: u, idx: item.idx}:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+			}
+			return nil
+		})
+	}
+
+	sender, receiver := stream.Pipe[U](0)
+
+	go func() {
+		iter := iterator.Map(
+			xsort.Merge(
+				func(a, b valueAndIndex[U]) bool {
+					return a.idx < b.idx
+				},
+				slices.Map(outs, func(c chan valueAndIndex[U]) iterator.Iterator[valueAndIndex[U]] {
+					return iterator.Chan(c)
+				})...,
+			),
+			func(x valueAndIndex[U]) U {
+				return x.value
+			},
+		)
+		for {
+			item, ok := iter.Next()
+			if !ok {
+				break
+			}
+			err := sender.Send(ctx, item)
+			if err != nil {
+				// could be either the receiver hung up or one of the above goroutines ran into a
+				// problem (which would've cancelled ctx already), either way it's time to bail out
+				cancel()
+				break
+			}
+		}
+		sender.Close(eg.Wait())
+	}()
+
+	return receiver
 }
--- parallel/parallel_go1.18_test.go
+++ parallel/parallel_go1.18_test.go
@@ -4,14 +4,19 @@
 package parallel
 
 import (
+	"context"
+	"errors"
 	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/bradenaw/juniper/iterator"
+	"github.com/bradenaw/juniper/stream"
 )
 
 func TestMap(t *testing.T) {
-	ints := []int{1, 2, 3, 4, 5}
+	ints := []int{0, 1, 2, 3, 4}
 	strs := Map(
 		2, // parallelism
 		ints,
@@ -19,5 +24,60 @@ func TestMap(t *testing.T) {
 			return strconv.Itoa(i)
 		},
 	)
-	require.Equal(t, strs, []string{"1", "2", "3", "4", "5"})
+	require.Equal(t, []string{"0", "1", "2", "3", "4"}, strs)
+}
+
+func TestMapIterator(t *testing.T) {
+	strs := MapIterator(
+		iterator.Counter(5),
+		2, // parallelism
+		0, // bufferSize
+		func(i int) string {
+			return strconv.Itoa(i)
+		},
+	)
+	require.Equal(t, []string{"0", "1", "2", "3", "4"}, iterator.Collect(strs))
+}
+
+func TestMapStream(t *testing.T) {
+	strsStream := MapStream(
+		stream.FromIterator(iterator.Counter(5)),
+		2, // parallelism
+		0, // bufferSize
+		func(ctx context.Context, i int) (string, error) {
+			return strconv.Itoa(i), nil
+		},
+	)
+	strs, err := stream.Collect(context.Background(), strsStream)
+	require.NoError(t, err)
+	require.Equal(t, []string{"0", "1", "2", "3", "4"}, strs)
+}
+
+func TestMapStreamError(t *testing.T) {
+	sender, receiver := stream.Pipe[int](0)
+	strsStream := MapStream(
+		receiver,
+		2, // parallelism
+		0, // bufferSize
+		func(ctx context.Context, i int) (string, error) {
+			return strconv.Itoa(i), nil
+		},
+	)
+
+	oopsError := errors.New("oops")
+
+	err := sender.Send(context.Background(), 0)
+	require.NoError(t, err)
+	err = sender.Send(context.Background(), 1)
+	require.NoError(t, err)
+	sender.Close(oopsError)
+
+	for {
+		_, err := strsStream.Next(context.Background())
+		if err == nil {
+			continue
+		}
+		require.Equal(t, oopsError, err)
+		break
+	}
 }
