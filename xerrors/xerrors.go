beecf1003d5139fb008bf8f47bcc1c37f7ba139b
Braden Walker <bradenawalker@gmail.com>
2022-01-19 17:50:12 -0800

    remove caching in xerrors.withStack.Error so it's not data racy

--- xerrors/xerrors.go
+++ xerrors/xerrors.go
@@ -9,33 +9,28 @@ import (
 type withStack struct {
 	inner error
 	pc    []uintptr
-	s     string
 }
 
 func (err withStack) Error() string {
-	if err.s == "" {
-		var sb strings.Builder
-		frames := runtime.CallersFrames(err.pc)
-		_, _ = sb.WriteString(err.inner.Error())
-		_, _ = sb.WriteString("\n\n")
-		for {
-			frame, more := frames.Next()
+	var sb strings.Builder
+	frames := runtime.CallersFrames(err.pc)
+	_, _ = sb.WriteString(err.inner.Error())
+	_, _ = sb.WriteString("\n\n")
+	for {
+		frame, more := frames.Next()
 
-			_, _ = sb.WriteString(frame.Function)
-			_, _ = sb.WriteString("(...)\n        ")
-			_, _ = sb.WriteString(frame.File)
-			_, _ = sb.WriteString(":")
-			_, _ = sb.WriteString(strconv.Itoa(frame.Line))
-			_, _ = sb.WriteString("\n")
+		_, _ = sb.WriteString(frame.Function)
+		_, _ = sb.WriteString("(...)\n        ")
+		_, _ = sb.WriteString(frame.File)
+		_, _ = sb.WriteString(":")
+		_, _ = sb.WriteString(strconv.Itoa(frame.Line))
+		_, _ = sb.WriteString("\n")
 
-			if !more {
-				break
-			}
+		if !more {
+			break
 		}
-		err.s = sb.String()
-		err.pc = nil
 	}
-	return err.s
+	return sb.String()
 }
 
 func (err withStack) Unwrap() error {
