c54e7b6451c946fecf9cb8292cba405a3d13dbdc
Braden Walker <bradenawalker@gmail.com>
2022-01-28 23:58:49 -0800

    double-space docs/index.md

--- gen_docs.sh
+++ gen_docs.sh
@@ -6,13 +6,14 @@ GOTOOL="$GOROOT/bin/go"
 
 rm -r docs/
 mkdir -p docs/
-echo "" > docs/index.md
+echo -n "" > docs/index.md
 
 pkgs="$($GOTOOL list ./... | sed "s/^github.com\/bradenaw\/juniper\///g" | grep -v "internal")"
 
 while read pkg; do
     mkdir -p "$(dirname "docs/$pkg")"
     echo "[$pkg]($pkg.md)" >> docs/index.md
+    echo >> docs/index.md
 done <<< "$pkgs"
 
 xargs -I{} -P12 --verbose $GOTOOL run github.com/robertkrimen/godocdown/godocdown@latest --output "docs/{}.md" {} <<< "$pkgs"
