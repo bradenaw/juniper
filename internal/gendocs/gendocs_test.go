e367f53e1a9b6eb5093e392326ec06e9332893e1
Braden Walker <bradenawalker@gmail.com>
2022-01-29 15:20:14 -0800

    looks semi-successful at generating

---
+++ internal/gendocs/gendocs_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/bradenaw/juniper/internal/require2"
+)
+
+func TestNonLocalSymbolLink(t *testing.T) {
+	require2.Equal(
+		t,
+		nonLocalSymbolLink(
+			"github.com/bradenaw/juniper/container/tree",
+			"github.com/bradenaw/juniper/iterator",
+			"Iterator",
+		),
+		"../iterator.md#Iterator",
+	)
+	require2.Equal(
+		t,
+		nonLocalSymbolLink(
+			"github.com/bradenaw/juniper/container/tree",
+			"github.com/bradenaw/juniper/internal/fuzz",
+			"Operations",
+		),
+		"../internal/fuzz.md#Operations",
+	)
+	require2.Equal(
+		t,
+		nonLocalSymbolLink(
+			"github.com/bradenaw/juniper/xmath",
+			"github.com/bradenaw/juniper/xmath/xrand",
+			"Sample",
+		),
+		"xmath/xrand.md#Sample",
+	)
+	require2.Equal(
+		t,
+		nonLocalSymbolLink(
+			"github.com/bradenaw/juniper/xmath/xrand",
+			"github.com/bradenaw/juniper/xmath",
+			"Min",
+		),
+		"../xmath.md#Min",
+	)
+}
