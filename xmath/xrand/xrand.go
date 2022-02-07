9b5013d4ddd60f5449d2d5b635823b262fe95db5
Braden Walker <bradenawalker@gmail.com>
2022-02-06 20:37:52 -0800

    interface indirection from 1.17

--- xmath/xrand/xrand.go
+++ xmath/xrand/xrand.go
@@ -1,7 +1,6 @@
 package xrand
 
 import (
-	"math"
 	"math/rand"
 )
 
@@ -35,61 +34,4 @@ func Sample(n int, k int) []int {
 // be passed to Shuffle.
 func RSample(r *rand.Rand, n int, k int) []int {
 	return rSample(r, n, k)
-}
-
-func rSample[R randRand](r R, n int, k int) []int {
-	out := make([]int, k)
-	samp := newSampler(r, k)
-	for {
-		next, replace := samp.Next()
-		if next >= n {
-			break
-		}
-		out[replace] = next
-	}
-	if n < k {
-		out = out[:n]
-	}
-	return out
-}
-
-type sampler[R randRand] struct {
-	i     int
-	first bool
-	w     float64
-	k     int
-	r     R
-}
-
-func newSampler[R randRand](r R, k int) sampler[R] {
-	return sampler[R]{
-		i:     0,
-		first: true,
-		w:     math.Exp(math.Log(r.Float64()) / float64(k)),
-		k:     k,
-		r:     r,
-	}
-}
-
-// Returns (next, replace) such that next is always increasing, and that the input item at index
-// next (if there is one) should replace the reservoir item at index replace.
-//
-// As such, for the first k iterations, always returns (i, i) to build the reservoir.
-func (s *sampler[R]) Next() (int, int) {
-	if s.i < s.k {
-		j := s.i
-		s.i++
-		return j, j
-	}
-	if s.first && s.i == s.k {
-		s.i--
-		s.first = false
-	}
-	skip := math.Floor(math.Log(s.r.Float64()) / math.Log(1-s.w))
-	if math.IsInf(skip, 0) || math.IsNaN(skip) {
-		return math.MaxInt, 0
-	}
-	s.i += int(skip) + 1
-	s.w *= math.Exp(math.Log(s.r.Float64()) / float64(s.k))
-	return s.i, s.r.Intn(s.k)
 }
--- xmath/xrand/xrand_go1.18.go
+++ xmath/xrand/xrand_go1.18.go
@@ -4,6 +4,7 @@ package xrand
 
 import (
 	"context"
+	"math"
 	"math/rand"
 
 	"github.com/bradenaw/juniper/iterator"
@@ -166,4 +167,61 @@ func rSampleSlice[T any, R randRand](r R, a []T, k int) []T {
 		out = out[:len(a)]
 	}
 	return out
+}
+
+func rSample[R randRand](r R, n int, k int) []int {
+	out := make([]int, k)
+	samp := newSampler(r, k)
+	for {
+		next, replace := samp.Next()
+		if next >= n {
+			break
+		}
+		out[replace] = next
+	}
+	if n < k {
+		out = out[:n]
+	}
+	return out
+}
+
+type sampler[R randRand] struct {
+	i     int
+	first bool
+	w     float64
+	k     int
+	r     R
+}
+
+func newSampler[R randRand](r R, k int) sampler[R] {
+	return sampler[R]{
+		i:     0,
+		first: true,
+		w:     math.Exp(math.Log(r.Float64()) / float64(k)),
+		k:     k,
+		r:     r,
+	}
+}
+
+// Returns (next, replace) such that next is always increasing, and that the input item at index
+// next (if there is one) should replace the reservoir item at index replace.
+//
+// As such, for the first k iterations, always returns (i, i) to build the reservoir.
+func (s *sampler[R]) Next() (int, int) {
+	if s.i < s.k {
+		j := s.i
+		s.i++
+		return j, j
+	}
+	if s.first && s.i == s.k {
+		s.i--
+		s.first = false
+	}
+	skip := math.Floor(math.Log(s.r.Float64()) / math.Log(1-s.w))
+	if math.IsInf(skip, 0) || math.IsNaN(skip) {
+		return math.MaxInt, 0
+	}
+	s.i += int(skip) + 1
+	s.w *= math.Exp(math.Log(s.r.Float64()) / float64(s.k))
+	return s.i, s.r.Intn(s.k)
 }
