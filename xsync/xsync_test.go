c4138890a8c9ffdb04f27fbfc2b8cd7286434258
Braden Walker <bradenawalker@gmail.com>
2023-11-24 21:04:30 -0500

    add xsync.Watchable

--- xsync/xsync_test.go
+++ xsync/xsync_test.go
@@ -97,3 +97,31 @@ func TestGroup(t *testing.T) {
 	// Jank, but just in case we'd be safe from the above panic just because the test is over.
 	time.Sleep(200 * time.Millisecond)
 }
+
+func ExampleWatchable() {
+	start := time.Now()
+
+	var w Watchable[int]
+	w.Set(0)
+	go func() {
+		for i := 1; i < 20; i++ {
+			w.Set(i)
+			fmt.Printf("set %d at %s\n", i, time.Since(start).Round(time.Millisecond))
+			time.Sleep(5 * time.Millisecond)
+		}
+	}()
+
+	for {
+		v, changed := w.Value()
+		if v == 19 {
+			return
+		}
+
+		fmt.Printf("observed %d at %s\n", v, time.Since(start).Round(time.Millisecond))
+
+		// Sleep for longer between iterations to show that we don't slow down the setter.
+		time.Sleep(17 * time.Millisecond)
+
+		<-changed
+	}
+}
